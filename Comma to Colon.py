a5212f5f808c5d120bb90042b800bfd1b34f3e9f
Myles WInstone <myles@writeandweb.uk>
2019-03-06 19:13:51 +0000

    Added another character to verbotten list
    Set the . character to be replaced by - in filenames

--- Comma to Colon.py
+++ Comma to Colon.py
@@ -2,12 +2,13 @@
 
 # Comma to Colon
 # A script to turn a CSV file into a Kirby CMS content file.
-# Buy a Kirby CMS licence: https://getkirby.com/buy
+# Try Kirby for free then buy a licence: https://getkirby.com/
 
 def filesafe(filename): # Create a Kirby safe filename
     safename = filename.replace(' ','-')
     safename = safename.replace('&','-')
     safename = safename.replace(',','-')
+    safename = safename.replace('.','-')
     safename = safename.replace("'",'-').lower()
     return safename
 
@@ -16,7 +17,7 @@ try: # For importing the CSV module
 except:
     print("CSV module is required")
     exit()
-try:
+try: # os required to create the individual folders
     import os
 except:
     print('Needs OS module')
@@ -32,14 +33,14 @@ except:
 filecsv = Tk()
 filecsv.filename = filedialog.askopenfilename(initialdir = "~/",title = "Select csv file to open",filetypes = (("CSV files","*.csv"),("all files","*.*")))
 print ("Opening file " + filecsv.filename)
-cn = input("What is the name of the Kirby blueprint being used? ")
+cn = input("What is the name of the Kirby blueprint being used (without the .yml extension)? ")
 blueprint = cn + '.txt'
 directory = filedialog.askdirectory(title = "Select where to save")
 # Start work on the CSV file
 sep = '\n\n----\n\n'
 with open(filecsv.filename, 'r') as f: # Open the file
     worklist = csv.DictReader(f)
-    for row in worklist:
+    for row in worklist: # Turn each row into a Kirby content txt file
         line = ''
         title = row['title'] # title is the one heading that must be present
         filename = filesafe(title) + '/' + blueprint # Create a Kirby safe folder and use blueprint name
@@ -47,7 +48,7 @@ with open(filecsv.filename, 'r') as f: # Open the file
             line += val + ': ' + item + sep
         fullfile = directory + '/' + filename
         os.makedirs(os.path.dirname(fullfile), exist_ok=True)
-        with open(fullfile,'w+') as sf: # Write the blueprint file
+        with open(fullfile,'w+') as sf: # Write the content txt file
             sf.write(line)
 print("Finished")
 exit()
