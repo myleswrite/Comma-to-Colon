d05d63d000686e9c6a8486f752fc327fae718f6f
Myles Winstone <myles@writeandweb.uk>
2024-10-13 13:05:37 +0100

    Updated readme and main .py file

--- Comma to Colon.py
+++ Comma to Colon.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 # Comma to Colon
 # A script to turn a CSV file into a folder of Kirby CMS content files.
 # Try Kirby for free: https://getkirby.com/
 
-
-def filesafe(filename): # Create a Kirby safe filename
+def file_safe(filename): # Create a Kirby safe filename
     safename = filename.replace(' ','-')
     safename = safename.replace('&','-')
     safename = safename.replace(',','-')
@@ -13,6 +14,27 @@ def filesafe(filename): # Create a Kirby safe filename
     safename = safename.replace("%",'-')
     safename = safename.lower()
     return safename
+
+def split_file(file, blueprint, directory):
+    sep = '\n\n----\n\n'
+    with open(file, 'r') as f: # Open the file
+        worklist = csv.DictReader(f)
+        for row in worklist: # Turn each row into a Kirby content txt file
+            line = ''
+            try:
+                title = row['title'] # title is the one heading that must be present
+                filename = file_safe(title) + '/' + blueprint # Create a Kirby safe folder and use blueprint name
+                print('Creating ' + file_safe(title) + '.txt')
+            except:
+                print("No 'title' heading found so stopping. 'title' must be a heading and written in lowercase for the conversion to work.")
+                exit()
+            for val,item in row.items(): # Go through each column of each row in the CSV to create a Kirby file.
+                line += val + ': ' + item + sep
+                fullfile = directory + '/' + filename
+                os.makedirs(os.path.dirname(fullfile), exist_ok=True)
+                with open(fullfile,'w+') as sf: # Write the content txt file
+                    sf.write(line)
+    return
 
 try: # For importing the CSV module
     import csv
@@ -36,28 +58,22 @@ print("Select the CSV file to open using the file browser")
 filecsv = Tk()
 filecsv.filename = filedialog.askopenfilename(initialdir = "~/",title = "Select csv file to open",filetypes = (("CSV files","*.csv"),("all files","*.*")))
 print ("Opening file " + filecsv.filename)
-cn = input("What is the name of the Kirby blueprint being used (without the .yml extension)? ")
-blueprint = cn + '.txt'
-print("Select the folder to save files into using the file browser")
-directory = filedialog.askdirectory(title = "Select where to save")
-# Start work on the CSV file
-sep = '\n\n----\n\n'
-with open(filecsv.filename, 'r') as f: # Open the file
-    worklist = csv.DictReader(f)
-    for row in worklist: # Turn each row into a Kirby content txt file
-        line = ''
-        try:
-            title = row['title'] # title is the one heading that must be present
-            filename = filesafe(title) + '/' + blueprint # Create a Kirby safe folder and use blueprint name
-            print('Creating ' + filesafe(title) + '.txt')
-        except:
-            print("No 'title' item found so stopping. 'title' must be a heading and written in lowercase for the conversion to work.")
-            exit()
-        for val,item in row.items(): # Go through each column of each row in the CSV to create a Kirby file.
-            line += val + ': ' + item + sep
-        fullfile = directory + '/' + filename
-        os.makedirs(os.path.dirname(fullfile), exist_ok=True)
-        with open(fullfile,'w+') as sf: # Write the content txt file
-            sf.write(line)
+if os.path.isfile(filecsv.filename) == False:
+    print("No file found")
+    exit()
+try:
+    cn = input("What is the name of the Kirby blueprint being used (without the .yml extension)? ")
+    blueprint = cn + '.txt'
+except:
+    print("Error entering Kirby blueprint name")
+    exit()
+try:
+    print("Select the folder to save files into using the file browser")
+    directory = filedialog.askdirectory(title = "Select where to save")
+except:
+    print("Error selecting save folder")
+    exit()
+# Work on the CSV file
+split_file(filecsv.filename, blueprint, directory)
 print("Finished")
 exit()
